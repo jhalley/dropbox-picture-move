311991a0f76cc2a2aaff308b10c5253bfb33dda9
Jhalley de Castro <joseph.halley@gmail.com>
2016-08-15 18:22:27 +0800

    feat: Get files in /Camera Uploads. Get count of files in /Camera Uploads

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import config
 import dropbox
+from pprint import pprint as pp
 
 class DropboxPictureMover():
     def __init__(self, token):
-        self.dbx = dropbox.Dropbox(token)
+        self.client = dropbox.client.DropboxClient(token)
 
     def getCurrentAccount(self):
-        return self.dbx.users_get_current_account()
+        return self.client.account_info()
+
+    def getAllPicturesInCameraUploads(self):
+        return map(lambda x: x['path'], self.client.metadata('/Camera Uploads')['contents'])
+
+    def getCountOfPicturesInCameraUploads(self):
+        return len(self.client.metadata('/Camera Uploads'))
 
 if __name__ == '__main__':
     app = DropboxPictureMover(config.app['oauth']['token'])
-    print app.getCurrentAccount()
+    print app.getAllPicturesInCameraUploads()
+    #print app.getCountOfPicturesInCameraUploads()
+    #print app.getCurrentAccount()
